45e792733bfe0275dcfa3472f2983b32dbe6206f
kzhh1 <108645465+kzhh1@users.noreply.github.com>
2022-08-01 09:48:26 +0800

    test

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -3,7 +3,7 @@ public class Item {
 	private String assetTag;
 	private String description;
 	private String dueDate;
-	private boolean isAvailable;
+	private boolean isAvailabl;
 
 	public Item(String assetTag, String description) {
 		this.assetTag = assetTag;
